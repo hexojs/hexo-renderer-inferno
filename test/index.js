4bbe229d0d12f8447d5d948d48d7433935a5c362
ppoffice <zrp1994@gmail.com>
2019-12-24 10:32:49 -0500

    chore: perf improvement via not copying str

--- test/index.js
+++ test/index.js
@@ -29,4 +29,33 @@ describe('Inferno renderer', () => {
             result.should.eq(expected);
         });
     });
+
+    it('root layout', () => {
+        const expected = '<!doctype html>\n<div><h1>Hello world!</h1></div>';
+        const path = pathFn.join(__dirname, 'fixture', 'simple.jsx');
+        fs.readFile(path).then(text => {
+            const result = r({text: text, path: path})({
+                content: '<h1>Hello world!</h1>',
+                layout: false,
+                filename: path,
+                view_dir: pathFn.dirname(path)
+            });
+            result.should.eq(expected);
+        });
+    });
+
+    it('non-root layout', () => {
+        const expected = '<html><h1>Hello world!</h1><p>From inferno.js</p></html>';
+        const path = pathFn.join(__dirname, 'fixture', 'complex.jsx');
+        fs.readFile(path).then(text => {
+            const result = r({text: text, path: path})({
+                title: 'Hello world!',
+                content: 'From inferno.js',
+                layout: 'complex',
+                filename: path,
+                view_dir: pathFn.dirname(path)
+            });
+            result.should.eq(expected);
+        });
+    });
 });
